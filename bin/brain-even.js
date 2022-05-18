ae4e22675e2d543b1cd2be5181f3f350a7d2774e
Alexander <the7alexander7@gamil.com>
2022-05-18 18:26:29 +0300

    fix jsCode

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,36 +1,5 @@
 #!/usr/bin/env node
 
-import readlineSync from 'readline-sync';
+import game from '../src/even.js';
 
-const randomNumber = Math.floor(Math.random * 100);
-const numbers = [randomNumber, randomNumber, randomNumber];
-
-const name = readlineSync.question('May I have your name? ');
-console.log(`Hello, ${name}!`);
-
-const answer = (text) => {
-  const result = console.log(text);
-  return result;
-};
-const rightAnswerNo = console.log(`'${answer}' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, ${name}!`);
-const rightAnswerYes = console.log(`'${answer}' is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, ${name}!`);
-
-const game = () => {
-  /* eslint-disable-next-line */
-  for (const number of numbers) {
-    console.log(`Question: ${number}`);
-    console.log(`Your answer: ${answer}`);
-    if ((answer === 'no' || answer !== 'yes') && number % 2 === 0) {
-      return rightAnswerYes;
-    } if ((answer === 'yes' || answer !== 'no') && number % 2 !== 0) {
-      return rightAnswerNo;
-    }
-    const correct = console.log('Correct!');
-    return correct;
-  }
-  const victory = console.log(`Congratulations, ${name}!`);
-  return victory;
-};
-
-console.log('Answer "yes" if the number is even, otherwise answer "no".');
 game();
--- src/even.js
+++ src/even.js
@@ -1,19 +1,20 @@
 import readlineSync from 'readline-sync';
 
-export const randomNumber = Math.floor(Math.random * 100);
-export const numbers = [randomNumber, randomNumber, randomNumber];
+const randomNumber = Math.floor(Math.random * 100);
+const numbers = [randomNumber, randomNumber, randomNumber];
 
-export const name = readlineSync.question('May I have your name? ');
-console.log(`Hello, ${name}!`);
-
-export const answer = (text) => {
+const answer = (text) => {
   const result = console.log(text);
   return result;
 };
-export const rightAnswerNo = console.log(`'${answer}' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, ${name}!`);
-export const rightAnswerYes = console.log(`'${answer}' is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, ${name}!`);
 
-export const game = () => {
+const game = () => {
+  console.log('Welcome to the Brain Games!');
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+  const rightAnswerNo = console.log(`'${answer}' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, ${name}!`);
+  const rightAnswerYes = console.log(`'${answer}' is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, ${name}!`);
   /* eslint-disable-next-line */
   for (const number of numbers) {
     console.log(`Question: ${number}`);
@@ -30,5 +31,4 @@ export const game = () => {
   return victory;
 };
 
-console.log('Answer "yes" if the number is even, otherwise answer "no".');
-game();
+export default game;
