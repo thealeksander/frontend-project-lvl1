8970941c733ba6f6a0e38b62b5e70707841994c3
Alexander <the7alexander7@gamil.com>
2022-05-18 19:00:02 +0300

    fix even.js

--- src/even.js
+++ src/even.js
@@ -3,25 +3,20 @@ import readlineSync from 'readline-sync';
 const randomNumber = Math.floor(Math.random * 100);
 const numbers = [randomNumber, randomNumber, randomNumber];
 
-const answer = (text) => {
-  const result = console.log(text);
-  return result;
-};
-
 const game = () => {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
   console.log('Answer "yes" if the number is even, otherwise answer "no".');
-  const rightAnswerNo = console.log(`'${answer}' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, ${name}!`);
-  const rightAnswerYes = console.log(`'${answer}' is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, ${name}!`);
   /* eslint-disable-next-line */
   for (const number of numbers) {
     console.log(`Question: ${number}`);
-    console.log(`Your answer: ${answer}`);
-    if ((answer === 'no' || answer !== 'yes') && number % 2 === 0) {
+    const useranswer = readlineSync.question('Your answer: ');
+    const rightAnswerNo = console.log(`'${useranswer}' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, ${name}!`);
+    const rightAnswerYes = console.log(`'${useranswer}' is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, ${name}!`);
+    if ((useranswer === 'no' || useranswer !== 'yes') && number % 2 === 0) {
       return rightAnswerYes;
-    } if ((answer === 'yes' || answer !== 'no') && number % 2 !== 0) {
+    } if ((useranswer === 'yes' || useranswer !== 'no') && number % 2 !== 0) {
       return rightAnswerNo;
     }
     const correct = console.log('Correct!');
