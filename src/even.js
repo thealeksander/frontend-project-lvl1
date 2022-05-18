47be9684134164e4c30ab39356c0ed496c2d1fb1
Alexander <the7alexander7@gamil.com>
2022-05-18 20:55:38 +0300

    needs solution

--- src/even.js
+++ src/even.js
@@ -1,7 +1,9 @@
 import readlineSync from 'readline-sync';
 
-const randomNumber = Math.floor(Math.random() * 100);
-const numbers = [randomNumber, randomNumber, randomNumber];
+const randomNumber1 = Math.floor(Math.random() * 100);
+const randomNumber2 = Math.floor(Math.random() * 100);
+const randomNumber3 = Math.floor(Math.random() * 100);
+const numbers = [randomNumber1, randomNumber2, randomNumber3];
 
 const isEven = (even) => {
   if (even % 2 === 0) {
@@ -16,13 +18,13 @@ const game = () => {
   console.log(`Hello, ${name}!`);
   console.log('Answer "yes" if the number is even, otherwise answer "no".');
   /* eslint-disable-next-line */
-  for (const number of numbers) {
-    console.log(`Question: ${number}`);
+  for (const randomNumber of numbers) {
+    console.log(`Question: ${randomNumber}`);
     const useranswer = readlineSync.question('Your answer: ');
-    const correctAnswer = console.log(`'${useranswer}' is wrong answer ;(. Correct answer was ${isEven(number)}.\nLet's try again, ${name}!`);
-    if (useranswer === 'no' && isEven(number) === 'yes') {
+    const correctAnswer = console.log(`'${useranswer}' is wrong answer ;(. Correct answer was ${isEven(randomNumber)}.\nLet's try again, ${name}!`);
+    if (useranswer === 'no' && isEven(randomNumber) === 'yes') {
       return correctAnswer;
-    } if (useranswer === 'yes' && isEven(number) === 'no') {
+    } if (useranswer === 'yes' && isEven(randomNumber) === 'no') {
       return correctAnswer;
     }
     console.log('Correct!');
