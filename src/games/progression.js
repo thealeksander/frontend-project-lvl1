bd560271d6426cf311c59f0cc7272ff73745c017
Alexander <the7alexander7@gamil.com>
2022-06-08 17:02:46 +0300

    finish gcdGame

--- src/games/progression.js
+++ src/games/progression.js
@@ -5,30 +5,34 @@ const rulesOfGame = 'What number is missing in the progression?';
 
 const getArrey = () => {
   const arrey = [];
-  const number = _.random(0, 50); // if you'll get error, see here
-  const interval = _.random(0, 8);
-  const length = _.random(4, 10);
+  let number = _.random(0, 50);
+  const interval = _.random(1, 8);
+  const length = _.random(5, 10);
   for (let i = 0; i < length; i += 1) {
-    arrey.push(number + interval);
+    arrey.push(number);
+    number += interval;
   }
   return arrey;
 };
 
 const getQuestion = () => {
   const arreyWhithSpace = getArrey();
-  const randomPlace = _.random(0, arreyWhithSpace.length - 1);
-  let numberOfAnswer = arreyWhithSpace[randomPlace];
-  numberOfAnswer = '..';
-  return arreyWhithSpace;
+  const randomSpace = _.random(0, arreyWhithSpace.length - 1);
+  arreyWhithSpace[randomSpace] = '..';
+  const result = arreyWhithSpace.join(' ');
+  return result;
 };
 
 const getAnswer = () => {
-  
+  const irr = getQuestion();
+  const line = irr.split(' ');
+  const result = line.indexOf('..');
+  return result;
 };
 
 const generateRound = () => {
   const question = `${getQuestion()}`;
-  const answer = String(gcd(number1, number2, number3, number4, number5));
+  const answer = String(getAnswer());
   return [question, answer];
 };
 
