d9ebf0369a98fbf98faafd4f67a75218f7d150db
Alexander <the7alexander7@gamil.com>
2022-06-09 19:52:55 +0300

    finish primeGame

--- src/games/prime.js
+++ src/games/prime.js
@@ -1,12 +1,24 @@
 import _ from 'lodash';
 import logicBrainGames from '../index.js';
 
-const rulesOfGame = 'What number is missing in the progression?';
+const rulesOfGame = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+const isPrime = (randomNumber) => {
+  let result = '';
+  for (let i = 2; i < randomNumber; i += 1) {
+    if (randomNumber % i === 0) {
+      result = 'false';
+    } else {
+      result = 'true';
+    }
+  }
+  return result;
+};
 
 const generateRound = () => {
   const randomNumber = _.random(2, 100);
   const question = String(randomNumber);
-  const answer = String(randomNumber);
+  const answer = (isPrime());
   return [question, answer];
 };
 
