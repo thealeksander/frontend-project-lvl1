499f266c7b32e000f722b7e4e5e6081802d6b639
Alexander <the7alexander7@gamil.com>
2022-06-09 20:21:06 +0300

    exactly finish primeGame

--- src/games/prime.js
+++ src/games/prime.js
@@ -4,21 +4,18 @@ import logicBrainGames from '../index.js';
 const rulesOfGame = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const isPrime = (randomNumber) => {
-  let result = '';
   for (let i = 2; i < randomNumber; i += 1) {
     if (randomNumber % i === 0) {
-      result = 'false';
-    } else {
-      result = 'true';
+      return false;
     }
   }
-  return result;
+  return true;
 };
 
 const generateRound = () => {
-  const randomNumber = _.random(2, 100);
+  const randomNumber = _.random(1, 100);
   const question = String(randomNumber);
-  const answer = (isPrime());
+  const answer = isPrime(randomNumber) ? 'yes' : 'no';
   return [question, answer];
 };
 
