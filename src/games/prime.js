f00442506adb6082184f7a82b1bcdc21fc5073e5
Alexander <the7alexander7@gamil.com>
2022-06-09 09:30:12 +0300

    add prime.js

---
+++ src/games/prime.js
@@ -0,0 +1,29 @@
+import logicBrainGames from '../index.js';
+
+const rulesOfGame = 'What number is missing in the progression?';
+
+const getArrey = () => {
+  const arrey = [];
+  let number = _.random(0, 50);
+  const interval = _.random(1, 8);
+  const length = _.random(5, 10);
+  for (let i = 0; i < length; i += 1) {
+    arrey.push(number);
+    number += interval;
+  }
+  return arrey;
+};
+
+const generateRound = () => {
+  const progression = getArrey();
+  const randomSpace = _.random(0, progression.length - 1);
+  const rightNumber = progression[randomSpace];
+  progression[randomSpace] = '..';
+  const question = progression.join(' ');
+  const answer = String(rightNumber);
+  return [question, answer];
+};
+
+const primeGame = () => logicBrainGames(rulesOfGame, generateRound);
+
+export default primeGame;
