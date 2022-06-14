4a52f6f69c5b7d2288e03d3072e31f5d4b976bc2
Alexander <the7alexander7@gamil.com>
2022-06-14 18:29:59 +0300

    fix even.js

--- src/games/even.js
+++ src/games/even.js
@@ -1,40 +1,22 @@
-import readlineSync from 'readline-sync';
 import getRandomInt from '../utils.js';
+import logicBrainGames from '../index.js';
 
-const randomNumber1 = getRandomInt(0, 100);
-const randomNumber2 = getRandomInt(0, 100);
-const randomNumber3 = getRandomInt(0, 100);
-const numbers = [randomNumber1, randomNumber2, randomNumber3];
+const rulesOfGame = 'Answer "yes" if the number is even, otherwise answer "no".';
 
-const isEven = (even) => {
-  if (even % 2 === 0) {
+const isEven = (number) => {
+  if (number % 2 === 0) {
     return 'yes';
   }
   return 'no';
 };
 
-const game = () => {
-  console.log('Welcome to the Brain Games!');
-  const name = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${name}!`);
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
-  /* eslint-disable-next-line */
-  for (const randomNumber of numbers) {
-    console.log(`Question: ${randomNumber}`);
-    const useranswer = readlineSync.question('Your answer: ');
-    if (useranswer === 'no' && isEven(randomNumber) === 'yes') {
-      return console.log(`'${useranswer}' is wrong answer ;(. Correct answer was ${isEven(randomNumber)}.\nLet's try again, ${name}!`);
-    }
-    if (useranswer === 'yes' && isEven(randomNumber) === 'no') {
-      return console.log(`'${useranswer}' is wrong answer ;(. Correct answer was ${isEven(randomNumber)}.\nLet's try again, ${name}!`);
-    }
-    if (useranswer !== 'yes' && useranswer !== 'no') {
-      return console.log(`'${useranswer}' is wrong answer ;(. Correct answer was ${isEven(randomNumber)}.\nLet's try again, ${name}!`);
-    }
-    console.log('Correct!');
-  }
-  const victory = console.log(`Congratulations, ${name}!`);
-  return victory;
+const generateRound = () => {
+  const number = getRandomInt(0, 100);
+  const question = `${number}`;
+  const answer = isEven(number);
+  return [question, answer];
 };
+
+const game = () => logicBrainGames(rulesOfGame, generateRound);
 
 export default game;
