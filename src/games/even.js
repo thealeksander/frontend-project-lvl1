4e470b2f2a52b858cb773b68f3adcfde774fce35
Alexander <the7alexander7@gamil.com>
2022-06-14 18:36:32 +0300

    fix function

--- src/games/even.js
+++ src/games/even.js
@@ -3,17 +3,12 @@ import logicBrainGames from '../index.js';
 
 const rulesOfGame = 'Answer "yes" if the number is even, otherwise answer "no".';
 
-const isEven = (number) => {
-  if (number % 2 === 0) {
-    return 'yes';
-  }
-  return 'no';
-};
+const isEven = (number) => number % 2 === 0;
 
 const generateRound = () => {
   const number = getRandomInt(0, 100);
   const question = `${number}`;
-  const answer = isEven(number);
+  const answer = isEven(number) ? 'yes' : 'no';
   return [question, answer];
 };
 
